cfe6a1107078c6677a24514bd7daaa789e006631
miolfo <mikko.o.forsman@gmail.com>
2017-11-23 21:33:50 +0200

    Progress

--- src/todo_list/core.clj
+++ src/todo_list/core.clj
@@ -2,7 +2,33 @@
   (require [ring.adapter.jetty :as jetty]
            [ring.middleware.reload :refer [wrap-reload]]
            [compojure.core :refer [defroutes GET]]
-           [compojure.route :refer [not-found]]))
+           [compojure.route :refer [not-found]]
+           [ring.handler.dump :refer [handle-dump]]))
+
+(def operands {"+" + "-" - "*" * ":" /})
+
+(defn hello
+  "Sample for parsing input parameters in url"
+  [request]
+  (let [name (get-in request [:route-params :name])]
+    {:status 200
+     :body (str "Hello " name)
+     :headers{}}))
+
+(defn calculator
+"A very simple calculator that can add, divide, subtract and multiply.  This is done through the magic of variable path elements."
+[request]
+(let [a  (Integer. (get-in request [:route-params :a]))
+      b  (Integer. (get-in request [:route-params :b]))
+      op (get-in request [:route-params :op])
+      f  (get operands op)]
+  (if f
+    {:status 200
+      :body (str "Calculated result: " (f a b))
+      :headers {}}
+    {:status 404
+      :body "Invalid operation"
+      :headers {}})))
 
 (defn welcome
 "Handler function for all requests"
@@ -24,11 +50,13 @@
   {:status 200
    :body (pr-str request)
    :headers{}})
-           
-(defroutes app 
+
+(defroutes app
   (GET "/" [] welcome)
   (GET "/goodbye" [] goodbye)
-  (GET "/request-info" [] request-info)
+  (GET "/request-info" [] handle-dump)
+  (GET "/hello/:name" [] hello)
+  (GET "/calculator/:a/:op/:b" [] calculator)
   (not-found "404 this is not the page you are looking for"))
 
 (defn -dev-main
