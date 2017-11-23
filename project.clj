f40e851073221c05b911ed75df2ca11dbbdaf515
miolfo <mikko.o.forsman@gmail.com>
2017-11-23 21:43:03 +0200

    Procfile and project.clj updates

--- project.clj
+++ project.clj
@@ -7,6 +7,8 @@
     [org.clojure/clojure "1.8.0"]
     [ring "1.6.3"]
     [compojure "1.3.4"]]
+  :min-lein-version "2.0.0"
+  :uberjar-name "todo-list.jar"
   :main todo-list.core
   :profiles {:dev
                 {:main todo-list.core/-dev-main}})
